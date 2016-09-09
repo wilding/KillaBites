77cb3b8d39983330004a62bc966be26c4fe2c3a1
wilding <wildingthrelfall@gmail.com>
2016-09-09 19:36:03 +0000

    new: initial Flask setup

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, joinedload_all
+from database_setup import Base, Ingredient
+
+app = Flask(__name__)
+
+# connect to database
+engine = create_engine('sqlite:///killabites.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+def homepage():
+    return "It's working!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8080)
