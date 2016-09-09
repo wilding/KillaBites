5ba3832f9adf2201e659468c671bae6e26cde6e9
wilding <wildingthrelfall@gmail.com>
2016-09-09 21:46:15 +0000

    new: add newIngredient view
    	- GET method returns html with form
    	- POST method
    		- takes form data and adds new ingredient to database
    		- flashes a confirmation message
    		- redirects to homepage

--- __init__.py
+++ __init__.py
@@ -16,6 +16,32 @@ session = DBSession()
 def homepage():
     return render_template('homepage.html')
 
+@app.route('/ingredient/add/', methods=['GET', 'POST'])
+def addIngredient():
+    if request.method == 'POST':
+        newingredient = Ingredient(
+            name=request.form['name'],
+            picture=request.form['picture'],
+            group=request.form['group'],
+            january=request.form['january'],
+            february=request.form['february'],
+            march=request.form['march'],
+            april=request.form['april'],
+            may=request.form['may'],
+            june=request.form['june'],
+            july=request.form['july'],
+            august=request.form['august'],
+            september=request.form['september'],
+            october=request.form['october'],
+            november=request.form['november'],
+            december=request.form['december'])
+        session.add(newingredient)
+        session.commit()
+        flash('Ingredient added!')
+        return redirect(url_for('homepage'))
+    else:
+        return render_template('newingredient.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8080)
