36abe2a2abb731384865a662e333e7081285b408
wilding <wildingthrelfall@gmail.com>
2016-09-25 14:09:01 -0700

    meta: update vagrant provisions

--- vagrant_provisions.sh
+++ vagrant_provisions.sh
@@ -2,6 +2,7 @@ apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
+apt-get -qqy install git
 pip install bleach
 pip install oauth2client
 pip install requests
