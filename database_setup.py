a25977be2da37db87e5677f873208dc65e333444
wilding <wildingthrelfall@gmail.com>
2016-09-28 23:25:52 -0700

    new: add Recipes
        - import Interval, ARRAY, and JSON column types
        - Recipe class/table has the following attributes/columns:
            - id
            - name
            - pictures
            - gif
            - sources
            - time
            - total_yield
            - calories
            - vegetarian
            - months
            - cuisines
            - groups
            - courses
            - occassions
            - ingredients
            - instructions
        - serialize() instance method returns a dict of the instance for easy jsonification

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,6 @@
 # Configuration
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Interval
+from sqlalchemy.dialects.postgresql import ARRAY, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -34,6 +35,52 @@ class Ingredient(Base):
             'storage': self.storage
         }
         return ingredient_dict
+
+
+class Recipe(Base):
+    """docstring for Recipe class"""
+    __tablename__ = 'recipe'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(500), nullable=False)
+    pictures = Column(ARRAY(String(9999)))
+    gif = Column(String(9999))
+    sources = Column(ARRAY(String(9999)))
+    time = Column(Interval)
+    total_yield = Column(String(500))
+    calories = Column(Integer)
+    vegetarian = Column(Boolean)
+    months = Column(ARRAY(Integer))
+    cuisines = Column(ARRAY(String(500)))
+    groups = Column(ARRAY(String(500)))
+    courses = Column(ARRAY(String(500)))
+    occasions = Column(ARRAY(String(500)))
+    ingredients = Column(ARRAY(String(9999)))
+    instructions = Column(JSON)
+
+    @property
+    def serialize(self):
+        """Instance method, returns a dictionary
+        with column names as keywords for the instance's
+        associated values. Output can easily be jsonified."""
+        recipe_dict = {
+            'name': self.name,
+            'id': self.id,
+            'pictures': self.pictures,
+            'gif': self.gif,
+            'sources': self.sources,
+            'time': self.time,
+            'total_yield': self.total_yield,
+            'calories': self.calories,
+            'vegetarian': self.vegetarian,
+            'months': self.months,
+            'cuisines': self.cuisines,
+            'groups': self.groups,
+            'courses': self.courses,
+            'occasions': self.occasions,
+            'ingredients': self.ingredients,
+            'instructions': self.instructions
+        }
+        return recipe_dict
 
 
 # # Class
