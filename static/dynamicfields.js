bc1e7e7f340a0218c1b089eca38dd60812569a99
wilding <wildingthrelfall@gmail.com>
2016-10-18 00:05:38 +0000

    redo: change 'add' button to 'add-alts'

--- static/dynamicfields.js
+++ static/dynamicfields.js
@@ -1,6 +1,6 @@
 $(function() {
     //when the Add Field button is clicked
-    $("#add").click(function (e) {
+    $("#add-alts").click(function (e) {
         //Append a new row of code to the "#items" div
         $("#items").append('<div><input name="alt" type="text" maxlength="500"><button class="delete" type="button">Delete</button></div>'); });
 
