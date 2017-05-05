48184a97be90fd14760f395e27ebdc3ab7eb64cf
wilding <wildingthrelfall@gmail.com>
2017-05-05 16:57:56 -0700

    new: add course switcher to homepage recipes

--- static/homepage.js
+++ static/homepage.js
@@ -5,4 +5,13 @@ $(function() {
         $(this).parent().toggleClass('active-ingredient');
     });
 
+
+    $('.course-switcher-option').click(function() {
+        $(this).siblings().removeClass('course-switcher-selected');
+        $(this).addClass('course-switcher-selected');
+        var course = $(this).text();
+        $('.homepage-courses-' + course.split(' ')[0]).siblings().css('display', 'none');
+        $('.homepage-courses-' + course.split(' ')[0]).css('display', 'block');
+    });
+
 });
